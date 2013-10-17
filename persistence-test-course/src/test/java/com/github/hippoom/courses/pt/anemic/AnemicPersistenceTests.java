fb81a169daf9c60d94d985d0f3f0d9ee579af7cf
hippoom <hippoom@gmail.com>
2013-10-17 11:18:01 +0800

    Add dbunit-spring-test example

---
+++ persistence-test-course/src/test/java/com/github/hippoom/courses/pt/anemic/AnemicPersistenceTests.java
@@ -0,0 +1,48 @@
+package com.github.hippoom.courses.pt.anemic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:persistence.xml",
+		"classpath:test-dbdeploy.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class,
+		TransactionalTestExecutionListener.class,
+		DbUnitTestExecutionListener.class })
+public class AnemicPersistenceTests {
+
+	@Autowired
+	private JdbcPendingOrderDao dao;
+
+	@DatabaseSetup(value = "classpath:data/anemic_pending_order_fixture.xml")
+	@ExpectedDatabase(value = "classpath:data/anemic_pending_order_expect.xml", assertionMode = DatabaseAssertionMode.NON_STRICT)
+	@Test
+	public void orderInserted() throws Throwable {
+		PendingOrderDto prototype = dao.findBy("2");
+
+		PendingOrderDto copy = copy(prototype, "3");
+
+		dao.save(copy);
+	}
+
+	private PendingOrderDto copy(PendingOrderDto prototype, String copyId) {
+		ModelMapper mapper = new ModelMapper();
+		PendingOrderDto copy = mapper.map(prototype, PendingOrderDto.class);
+		copy.setTrackingId(copyId);
+		return copy;
+	}
+}
